ab7fd953c509e3750cb3b0956f7798c75c6ff8d9
lemon-sh <pawel.stolarski7@gmail.com>
2021-10-31 19:46:50 +0100

    Update the serial input

--- src/app.hpp
+++ src/app.hpp
@@ -11,7 +11,7 @@ namespace App
 	enum class InputType
 	{
 		left, right,
-		d1, d2, d3, d4, d5, d6, d7, d8, d9,
+		d0, d1, d2, d3, d4, d5, d6, d7, d8, d9,
 		del, ret,
 		add, sub, mul, div,
 	};
--- src/main.cpp
+++ src/main.cpp
@@ -22,13 +22,19 @@ void loop() {
 	if ((input = Serial.read()) == -1) return;
 	switch ((char)input)
 	{
-	case 'l':
-		App::input(App::InputType::right);
-		break;
-	
-	case 'h':
-		App::input(App::InputType::left);
-		break;
+		case 'l': App::input(App::InputType::right); break;
+		case 'h': App::input(App::InputType::left); break;
+		case 'p': App::input(App::InputType::del); break;
+		case '0': App::input(App::InputType::d0); break;
+		case '1': App::input(App::InputType::d1); break;
+		case '2': App::input(App::InputType::d2); break;
+		case '3': App::input(App::InputType::d3); break;
+		case '4': App::input(App::InputType::d4); break;
+		case '5': App::input(App::InputType::d5); break;
+		case '6': App::input(App::InputType::d6); break;
+		case '7': App::input(App::InputType::d7); break;
+		case '8': App::input(App::InputType::d8); break;
+		case '9': App::input(App::InputType::d9); break;
 	}
 #endif
 }
